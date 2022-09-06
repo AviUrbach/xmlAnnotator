aa087317cfe57fb5907ffad211892894649043f4
AviUrbachWM <akurbach@email.wm.edu>
2022-09-06 01:04:24 -0400

    Implememnt string parsing in xml file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # xmlAnnotator
 CSci 435 Programming assignment #1
+
+To set up required modules, run "pip install -r requirements.txt"
+
+To use the annotator, run the python script annotate.py, giving the path to the input files as the first argument. For example, "python annotate.py C:\Users\aviku\Downloads\Programming-Assignment-Data"
--- annotate.py
+++ annotate.py
@@ -1,1 +1,56 @@
-print('hello world')
+import os
+import sys
+import png
+import numpy
+import fileinput
+
+try:
+    os.mkdir('output')
+except FileExistsError:
+    pass
+
+path = sys.argv[1] #input directiory from command line
+
+for file in os.listdir(path):
+    if file.endswith('.xml'): #only read each xml/png pair once
+        name = file[:-4]
+        print(name)
+
+        #read and process png file
+        reader = png.Reader(filename = os.path.join(path, name + '.png'))
+        width, height, rows, info = reader.read()
+        image = [numpy.uint16(row) for row in rows]
+        matrix = numpy.vstack(image)
+        print(width, height)
+        print(matrix)
+        print(len(matrix))
+        print(len(matrix[0]))
+
+        for line in fileinput.input(os.path.join(path, file)):
+            index = line.find('<node')
+            while index != -1: #find next node
+                index += 5
+                endnodeindex = line.find('>', index)
+                if line[endnodeindex-1] == '/':
+                    print(line[:endnodeindex])
+                    #this is a leaf node, time to do some ugly string parsing
+                    boundindex = line.find('bounds=', index)
+                    firstboundindex = line.find('[', boundindex)
+                    comma1index = line.find(',', firstboundindex)
+                    bah = line[firstboundindex+1:comma1index]
+                    x1 = int(bah)
+                    breakindex = line.find(']', comma1index)
+                    y1 = int(line[comma1index+1:breakindex])
+                    secondboundindex = line.find('[', breakindex)
+                    comma2index = line.find(',', secondboundindex)
+                    x2 = int(line[secondboundindex+1:comma2index])
+                    endindex = line.find(']',comma2index)
+                    y2 = int(line[comma2index+1:endindex])
+
+                    #draw box
+                    
+                index = line.find('<node', endnodeindex)
+                    
+
+        outfile = open(os.path.join('output', name + '_annotated.png'), 'wb')
+        #writer = png.Writer()
