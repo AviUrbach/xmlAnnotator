f6d30127f7bd1c54be9178b41cf8256180cdbbe5
AviUrbachWM <akurbach@email.wm.edu>
2022-09-06 01:45:56 -0400

    Add box drawing

--- annotate.py
+++ annotate.py
@@ -21,7 +21,6 @@ for file in os.listdir(path):
         width, height, rows, info = reader.read()
         matrix = numpy.vstack([row for row in rows])
         print(width, height)
-        print(matrix)
         print(len(matrix))
         print(len(matrix[0]))
 
@@ -31,7 +30,6 @@ for file in os.listdir(path):
                 index += 5
                 endnodeindex = line.find('>', index)
                 if line[endnodeindex-1] == '/':
-                    print(line[:endnodeindex])
                     #this is a leaf node, time to do some ugly string parsing
                     boundindex = line.find('bounds=', index)
                     firstboundindex = line.find('[', boundindex)
@@ -46,7 +44,35 @@ for file in os.listdir(path):
                     endindex = line.find(']',comma2index)
                     y2 = int(line[comma2index+1:endindex])
 
-                    #draw box
+                    #fix out of bounds error
+                    if x2 >= width:
+                        x2 = width - 1
+                    if y2 >= height:
+                        y2 = height - 1
+
+                    #draw box in red
+                    for x in range(x1, x2 + 1):
+                        #set red to 255 and blue, green to 0
+                        #note the *4 to account for the values r,g,b,a for each pixel
+                        matrix[y1][x * 4] = 255
+                        matrix[y1][x * 4 + 1] = 0
+                        matrix[y1][x * 4 + 2] = 0
+
+                        matrix[y2][x * 4] = 255
+                        matrix[y2][x * 4 + 1] = 0
+                        matrix[y2][x * 4 + 2] = 0
+
+                    for y in range(y1 + 1, y2):
+                        #set red to 255 and blue, green to 0
+                        #note the *4 to account for the values r,g,b,a for each pixel
+                        matrix[y][x1 * 4] = 255
+                        matrix[y][x1 * 4 + 1] = 0
+                        matrix[y][x1 * 4 + 2] = 0
+
+                        matrix[y][x2 * 4] = 255
+                        matrix[y][x2 * 4 + 1] = 0
+                        matrix[y][x2 * 4 + 2] = 0
+
                     
                 index = line.find('<node', endnodeindex)
                     
